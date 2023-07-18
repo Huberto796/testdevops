450cfb239840456ee50c23a72d2ed221126267a8
Huberto Solorzano <hsolorzano796@gmail.com>
2023-07-18 05:42:24 +0000

    v1

--- app/resources/fiori/webapp/Component.js
+++ app/resources/fiori/webapp/Component.js
@@ -2,8 +2,6 @@ sap.ui.define(['sap/fe/core/AppComponent'], function (AppComponent) {
     'use strict';
 
     
-
-    
     return AppComponent.extend("testdevops-fiori.Component", {
         metadata: {
             manifest: "json"
